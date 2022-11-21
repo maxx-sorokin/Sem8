d6a29aa2763fda9a6f7987b1e400092b0a1ea0cc
Maxim Sorokin <maximka773@mail.ru>
2022-11-21 23:59:19 +0300

    57 process

---
+++ 57/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного
+// массива. Частотный словарь содержит информацию о том,
+// сколько раз встречается элемент входных данных.
+
+Console.WriteLine("Введите количество строк");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите количество столбцов");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+
+int[,] FrequencyDictionary(int[,] array)
+{
+    int[,] result = new int[2, array.Length];
+    int index = 0;
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (Check(result, array[i, j]))
+            {
+                result[1, index]++;
+            }
+            else
+            {
+                result[0, index] = array[i, j];
+                result[1, index] = 1;
+            }
+        }
+    }
+}
+
+bool Check(int[,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        if (array[0, i] == number)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
